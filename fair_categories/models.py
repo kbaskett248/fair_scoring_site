30e4ee59581e1e87b396bce6dc261087bda33d27
Kenny Baskett <kbaskett248@gmail.com>
2016-08-07 14:03:51 -0400

    Adding active field to fair_categories models

--- fair_categories/models.py
+++ fair_categories/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Division(models.Model):
+    active = models.BooleanField(
+        help_text=('Inactivate the division instead of deleting it'),
+        default=True
+    )
     short_description = models.CharField(
         max_length=100,
         verbose_name='Division name'
@@ -13,6 +17,10 @@ class Division(models.Model):
 
 
 class Category(models.Model):
+    active = models.BooleanField(
+        help_text=('Inactivate the category instead of deleting it'),
+        default=True
+    )
     short_description = models.CharField(
         max_length=100,
         verbose_name='Category name'
@@ -23,6 +31,10 @@ class Category(models.Model):
 
 
 class Subcategory(models.Model):
+    active = models.BooleanField(
+        help_text=('Inactivate the subcategory instead of deleting it'),
+        default=True
+    )
     abbreviation = models.CharField(
         max_length=10,
         verbose_name='Abbreviation'
@@ -38,6 +50,10 @@ class Subcategory(models.Model):
 
 
 class Ethnicity(models.Model):
+    active = models.BooleanField(
+        help_text=('Inactivate the ethnicity instead of deleting it'),
+        default=True
+    )
     short_description = models.CharField(
         max_length=50,
         verbose_name='Ethnicity name')
