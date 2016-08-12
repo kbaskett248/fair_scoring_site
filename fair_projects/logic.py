26fc4c076259f4bd5b879797407c1e41edb0b503
Kenny Baskett <kbaskett248@gmail.com>
2016-08-11 20:42:07 -0400

    Improving project import
    Project import will also import students associated with those projects.

--- fair_projects/logic.py
+++ fair_projects/logic.py
@@ -1,13 +1,9 @@
-from collections import defaultdict
 import csv
-import itertools
 
-from django.db.models import Max
 from django.core.exceptions import ObjectDoesNotExist
 
-from .models import Project
-
-from fair_categories.models import Category, Subcategory, Division
+from .models import Project, Student, Teacher
+from fair_categories.models import Category, Subcategory, Division, Ethnicity
 
 IMPORT_DICT_KEYS = ('Timestamp', 'Project Title', 'Project Abstract',
                     'Project Category', 'Project Subcategory', 'Unused1',
@@ -34,23 +30,62 @@ def handle_project_import(file_):
     reader = csv.DictReader(contents[1:], fieldnames=IMPORT_DICT_KEYS, dialect=dialect)
 
     for row in reader:
-        cat = Category.objects.get(short_description__icontains=row['Project Category'])
-        try:
-            subcat = Subcategory.objects.get(category=cat,
-                                             short_description__icontains=row['Project Subcategory'])
-        except ObjectDoesNotExist:
-            continue
-
         if int(row['S1 Grade Level']) >= 9:
             div = high_div
         else:
             div = mid_div
 
-        project = Project(title=row['Project Title'],
-                          abstract=row['Project Abstract'],
-                          category=cat,
-                          subcategory=subcat,
-                          division=div
-                          )
-        project.save()
+        project = create_project(row['Project Title'], row['Project Abstract'], row['Project Category'],
+                                 row['Project Subcategory'], div)
+
+        if not project:
+            continue
+
+        for sn in range(1, 3):
+            create_student(row['S%s First Name' % sn],
+                           row['S%s Last Name' % sn],
+                           row['S%s Ethnicity' % sn],
+                           row['S%s Gender' % sn],
+                           row['S%s Teacher' % sn],
+                           row['S%s Grade Level' % sn],
+                           project)
+
+
+def create_project(title, abstract, cat_name, subcat_name, division):
+    cat = Category.objects.get(short_description__icontains=cat_name)
+    try:
+        subcat = Subcategory.objects.get(category=cat,
+                                         short_description__icontains=subcat_name)
+    except ObjectDoesNotExist:
+        return None
+
+    project = Project(title=title,
+                      abstract=abstract,
+                      category=cat,
+                      subcategory=subcat,
+                      division=division)
+    project.save()
+
+    return project
+
+def create_student(first_name, last_name, eth_name, gender, teacher_name, grade_level, project, email=None):
+    if not first_name:
+        return
+
+    ethnicity = Ethnicity.objects.get(short_description=eth_name)
+    teacher = Teacher.objects.get(user__last_name=teacher_name)
+
+    student = Student(first_name=first_name,
+                      last_name=last_name,
+                      ethnicity=ethnicity,
+                      gender=gender,
+                      teacher=teacher,
+                      grade_level=grade_level,
+                      project=project)
+    if email:
+        student.email = email
+
+    student.save()
+
+    return student
 
