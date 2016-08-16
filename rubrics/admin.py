b1bd840f068316d4aa681139c5cee293e1fdc6f2
Kenny Baskett <kbaskett248@gmail.com>
2016-08-16 06:21:55 -0400

    Adding Question to the Admin site

--- rubrics/admin.py
+++ rubrics/admin.py
@@ -13,21 +13,16 @@ class QuestionInline(admin.StackedInline):
     model = Question
     can_delete = True
     ordering = ('order', )
-    inlines = (ChoiceInline, )
-
-    def get_formsets(self, request, obj=None):
-        for inline in self.get_inline_instances(request, obj):
-            if isinstance(inline, ChoiceInline):
-                if obj and obj.show_choices():
-                    yield inline.get_formset(request, obj)
-                else:
-                    continue
-            else:
-                yield inline.get_formset(request, obj)
 
 
 @admin.register(Rubric)
 class RubricAdmin(admin.ModelAdmin):
     model = Rubric
     inlines = (QuestionInline, )
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = (ChoiceInline, )
 
