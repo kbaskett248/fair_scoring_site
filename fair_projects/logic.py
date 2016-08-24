01a194497c20d72f0b0b13394bb49f0267ac4699
Kenny Baskett <kbaskett248@gmail.com>
2016-08-24 18:33:28 -0400

    Documenting judge assignment algorithm

--- fair_projects/logic.py
+++ fair_projects/logic.py
@@ -96,6 +96,20 @@ def create_student(first_name, last_name, eth_name, gender, teacher_name, grade_
 
 
 def assign_judges():
+    # 1. Assign judges to new projects
+    #    1. Search for projects with fewer than the minimum number of judges.
+    #    2. For each project:
+    #       1. Determine the number of judges the project needs.
+    #       2. Search for judges that can judge the project's category and division, sorted by the number of projects
+    #          ascending. Get the number of judges needed for the project.
+    #       3. Assign the judges to the project
+    # 2. Switch projects from judges that have too many projects
+    #    1. Count the number of projects in each category division group, the number of judges that can judge that
+    #       category and division, and calculate their quotient.
+    #    2. Compute the median number of projects assigned to all judges.
+    #    3. Compute the max of the median and the minimum number of projects per judge.
+    #    4. Search for judges that have more than this number of projects assigned
+    #    5.
     rubric = Rubric.objects.get(name='Judging Rubric')
     judge_set = Judge.objects.annotate(
         num_projects=Count('judginginstance'),
