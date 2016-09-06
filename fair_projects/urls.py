e96bdd0eb2fbb3f91c22faed74e0ed1d8aac28bd
Kenny Baskett <kbaskett248@gmail.com>
2016-09-05 22:45:36 -0400

    Improves views
    - Judge detail view is now based on username
    - more content moved into base template is now more extensible
    - bug fixed with navbar not expanding in small mode

--- fair_projects/urls.py
+++ fair_projects/urls.py
@@ -6,5 +6,5 @@ app_name = 'fair_projects'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<project_number>[0-9]+)/?$', views.detail, name='detail'),
-    url(r'^judge/(?P<judge_id>[0-9]+)/?$', views.judge_detail, name='judge_detail'),
+    url(r'^judge/(?P<judge_username>[A-Za-z._0-9]+)/?$', views.judge_detail, name='judge_detail'),
 ]
