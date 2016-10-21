1a7381c61074f7f428d1bdc50f1cb8a140d4e9ba
Kenny Baskett <kbaskett248@gmail.com>
2016-10-20 22:01:45 -0400

    fair_scoring_site-3 Write scoring algorithm to run when a score is submitted
    Adding additional hypothesis strategies to improve test cases.

--- rubrics/tests.py
+++ rubrics/tests.py
@@ -4,9 +4,9 @@ from hypothesis import settings
 from hypothesis.extra.django import TestCase as HypTestCase
 from hypothesis.extra.django.models import models
 from hypothesis.strategies import one_of, sampled_from, text, lists, fixed_dictionaries, integers, \
-    booleans, none
+    booleans, none, just, composite
 
-from rubrics.models import Rubric, Question, Choice
+from rubrics.models import Rubric, Question
 
 
 def create_simple_rubric():
@@ -28,6 +28,36 @@ def remove_none_values(dictionary: dict):
         if value is None:
             del dictionary[key]
     return dictionary
+
+
+def fixed_decimals():
+    return integers(min_value=0, max_value=999).map(lambda x: x / 1000)
+
+
+def sane_text(min_size=None, max_size=None, average_size=None):
+    return text(alphabet=[chr(i) for i in range(33, 126)],
+                min_size=min_size, max_size=max_size, average_size=average_size)
+
+
+@composite
+def rubric_with_questions(draw, min_questions: int=None, max_questions: int=None,
+                          average_questions: int=10):
+    rubric = draw(models(Rubric))
+    for _ in draw(lists(elements=none(), min_size=min_questions, average_size=average_questions, max_size=max_questions)):
+        question_type = draw(sampled_from(Question.available_types()))
+        if question_type in Question.CHOICE_TYPES:
+            weight = draw(fixed_decimals())
+        else:
+            weight = 0
+
+        draw(models(Question, rubric=just(rubric),
+                    short_description=sane_text(),
+                    question_type=just(question_type),
+                    weight=just(weight)))
+
+
+    return rubric
+
 
 
 add_question_dictionaries = fixed_dictionaries(
@@ -75,7 +105,7 @@ class RubricTests(HypTestCase):
                                  question_list)
 
 
-class QuestionTests(TestCase):
+class QuestionTests(HypTestCase):
     def test_is_allowed_type(self):
         self.assertTrue(Question.is_allowed_type(Question.LONG_TEXT))
         self.assertFalse(Question.is_allowed_type("Some Google Type"))
@@ -84,17 +114,21 @@ class QuestionTests(TestCase):
         self.assertTrue(Question.is_allowed_sort(Question.AUTO_SORT))
         self.assertFalse(Question.is_allowed_sort("Some Google Sort"))
 
-    def test_add_choice(self):
-        rubric = create_simple_rubric()
-        qs = Choice.objects.order_by('description')
-        self.assertQuerysetEqual(qs.all(),
-                                 ['<Choice: High>', '<Choice: Low>'])
-        self.assertQuerysetEqual(qs.filter(question__rubric=rubric).all(),
-                                 ['<Choice: High>', '<Choice: Low>'],
-                                 ordered=True)
-        self.assertQuerysetEqual(qs.filter(question__short_description='Scale Question').all(),
-                                 ['<Choice: High>', '<Choice: Low>'],
-                                 ordered=True)
+    @given(rubric_with_questions(min_questions=1))
+    def test_add_choice(self, rubric):
+        question = rubric.question_set.first()
+
+        def add_questions():
+            question.add_choice(1, 'Low')
+            question.add_choice(10, 'High')
+
+        if question.question_type == Question.LONG_TEXT:
+            with self.assertRaises(AttributeError):
+                add_questions()
+        else:
+            add_questions()
+            self.assertQuerysetEqual(question.choice_set.order_by('description').all(),
+                                     ['<Choice: High>', '<Choice: Low>'])
 
 
 class RubricResponseTests(TestCase):
