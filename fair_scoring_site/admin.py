673abd3fa562eb687386bf3fcabd4c24e91b149f
Kenny Baskett <kbaskett248@gmail.com>
2016-12-13 21:30:52 -0500

    fair_scoring_site-4 Compute specialty awards
    Adds Award Instances inline to the Project page

--- fair_scoring_site/admin.py
+++ fair_scoring_site/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.contrib import messages
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.urls.base import reverse
 from django.utils.html import format_html
 
 import awards.admin
+import fair_projects
 from awards.logic import InstanceBase, assign_awards
 from awards.models import Award, AwardInstance
 from fair_projects.logic import get_projects_sorted_by_score
+from fair_projects.models import Project
 
 
 def assign_awards_to_projects(modeladmin, request, queryset):
@@ -48,14 +51,20 @@ class AwardRuleInline(awards.admin.AwardRuleInline):
 
 
 class AwardInstanceInline(awards.admin.AwardInstanceInline):
-    readonly_fields = ('project_title', 'project_students')
+    readonly_fields = ('project_number', 'project_title', 'project_students')
 
-    def project_title(self, instance):
+    def admin_link(self, instance, text):
         return format_html(
             '<a href="{0}">{1}</a>',
-            reverse('admin:fair_projects_project_change', args=(instance.content_object.pk, )),
-            instance.content_object.title
+            reverse('admin:fair_projects_project_change', args=(instance.content_object.pk,)),
+            text
         )
+
+    def project_title(self, instance):
+        return self.admin_link(instance, instance.content_object.title)
+
+    def project_number(self, instance):
+        return self.admin_link(instance, instance.content_object.number)
 
     def project_students(self, instance):
         return ', '.join(str(student) for student in
@@ -78,3 +87,15 @@ class AwardAdmin(awards.admin.AwardAdmin):
 class AwardInstanceAdmin(admin.ModelAdmin):
     model = AwardInstance
     list_display = ('award', 'content_object')
+
+
+class ProjectAwardInline(GenericTabularInline):
+    model = AwardInstance
+
+
+admin.site.unregister(Project)
+
+
+@admin.register(Project)
+class ProjectAdmin(fair_projects.admin.ProjectAdmin):
+    inlines = (fair_projects.admin.StudentInline, ProjectAwardInline)
