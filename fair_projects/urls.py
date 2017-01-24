bb0fff65d2695f5e1372438f3be6a849b088376f
Kenny Baskett <kbaskett248@gmail.com>
2017-01-24 18:51:17 -0500

    Judge and teacher urls will now accommodate all usernames.

--- fair_projects/urls.py
+++ fair_projects/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     url(r'^(?P<project_number>[0-9]+)/delete/?$', views.ProjectDelete.as_view(), name='project_delete'),
     url(r'^(?P<project_number>[0-9]+)/feedback/student/(?P<student_id>[0-9]+)/?$', views.StudentFeedbackForm.as_view(), name='student_feedback_form'),
     url(r'^judges/?$', views.JudgeIndex.as_view(), name='judge_index'),
-    url(r'^judge/(?P<judge_username>[A-Za-z._0-9]+)/?$', views.JudgeDetail.as_view(), name='judge_detail'),
+    url(r'^judge/(?P<judge_username>[-+@._A-Za-z0-9]+)/?$', views.JudgeDetail.as_view(), name='judge_detail'),
     url(r'^judgingresponse/(?P<judginginstance_key>[0-9]+)/?$',
         views.JudgingInstanceDetail.as_view(), name='judging_instance_detail'),
     url(r'^judgingresponse/(?P<judginginstance_key>[0-9]+)/edit/?$',
         views.JudgingInstanceUpdate.as_view(), name='judging_instance_edit'),
-    url(r'^teacher/(?P<username>[A-Za-z._0-9]+)/?$', views.TeacherDetail.as_view(), name='teacher_detail'),
-    url(r'^teacher/(?P<username>[A-Za-z._0-9]+)/feedback/?$', views.TeacherStudentsFeedbackForm.as_view(), name='teacher_feedback'),
+    url(r'^teacher/(?P<username>[-+@._A-Za-z0-9]+)/?$', views.TeacherDetail.as_view(), name='teacher_detail'),
+    url(r'^teacher/(?P<username>[-+@._A-Za-z0-9]+)/feedback/?$', views.TeacherStudentsFeedbackForm.as_view(), name='teacher_feedback'),
     url(r'^results/?$', views.ResultsIndex.as_view(), name='project_results')
 ]
