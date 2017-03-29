4b7423c923c1017356de1fa6e7ceefa9f7e03539
Kenny Baskett <kbaskett248@gmail.com>
2017-03-29 10:15:01 -0400

    Adds a link to the signup form

--- judges/urls.py
+++ judges/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'fair_projects'
+app_name = 'judges'
 urlpatterns = [
     url(r'^signup/?$', views.JudgeCreateView.as_view(), name='judge_create')
 ]
