f2ec3eba82617b3e293a50b93fd5aa3b3931cddf
Kenny Baskett <kbaskett248@gmail.com>
2017-03-31 23:45:48 -0400

    Add name and email fields to sign up page

--- judges/views.py
+++ judges/views.py
@@ -6,9 +6,29 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm as UserForm
 
 from judges.models import Judge
+
+class UserCreationForm(UserForm):
+    """The summary line for a class docstring should fit on one line.
+
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+        attr2 (:obj:`int`, optional): Description of `attr2`.
+
+    """
+    Meta = UserForm.Meta
+    Meta.fields = Meta.fields + ('first_name', 'last_name', 'email')
+
 
 JudgeFormset = inlineformset_factory(User, Judge,
                                      fields=['user',
