b513b76c911af3d3ef1860fb458219d9cf179387
Kenny Baskett <kbaskett248@gmail.com>
2019-01-05 00:17:05 -0500

    Improve loading speed for Student Admin

--- fair_projects/admin.py
+++ fair_projects/admin.py
@@ -153,6 +153,11 @@ class StudentAdmin(ImportExportMixin, admin.ModelAdmin):
         except AttributeError:
             return "-"
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related(
+            'project', 'teacher', 'teacher__user')
+
+
 class TeacherInline(admin.StackedInline):
     model = Teacher
     can_delete = False
